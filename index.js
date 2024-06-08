d36ae42abca5267b818bc9dcfe1820e78952752d
Nekit2501 <131582700+Nekit2501@users.noreply.github.com>
2024-06-08 11:07:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+//Dom - дозвляє отримати доступ до HTML та іх замінюватми або провоити інщі дії
+//document- це HTML документ(сторінка)
+console.log(document);
+document.getElementById();
+document.getElementsByClassName();
