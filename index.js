b3360b32f2bfd1ec38e3eb31856f69a977c36b1d
Nekit2501 <nikitalavrenuk68@gmail.com>
2024-06-28 13:58:19 +0300

    1 dz

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-//Dom - дозвляє отримати доступ до HTML та іх замінюватми або провоити інщі дії
-//document- це HTML документ(сторінка)
-console.log(document);
-document.getElementById();
-document.getElementsByClassName();
+// const age = 16;
+// console.log(age);
+// const name = "Max";
+// console.log(name);
+// const isStudent = false;
+// console.log(isStudent)
+
+// const myNumber = 10 + 10;
+// console.log(myNumber)
+
+// const myNull = null;
+// console.log(myNull)
+
+// const result = prompt('',);
+// alert ('Привіт'+' '+result)
+
+// const resultConfirm = confirm('Ви впевненні в дії?')
+// console.log(resultConfirm)
+// if (resultConfirm) {
+//     const resultConfirm = alert('Дякую за підтвердження!')
+// }else{
+//     const resultConfirm = alert('Дію відмінено!')
+// }
+
+alert('Дія небезпечна! Підтвердить дію ');
+const alertConfirm = confirm('Підтвердить дію');
+if (alertConfirm) {
+    const resultConfirm = alert('Дякую за підтвердження!')
+}else{
+    const resultConfirm = alert('Дію відмінено!')
+}
